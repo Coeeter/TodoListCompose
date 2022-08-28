b2ea71175ec8d9719da5a23cde9d42770ba37870
Nasrullah <nasrullah01n@gmail.com>
2022-08-28 19:46:44 +0800

    Deleted unnecessary exception

--- app/src/main/java/com/example/todolistcompose/domain/exceptions/TodoNotFoundException.kt
+++
@@ -1,3 +0,0 @@
-package com.example.todolistcompose.domain.exceptions
-
-class TodoNotFoundException(message: String) : Exception(message)
--- app/src/main/java/com/example/todolistcompose/domain/useCases/todos/GetTodoByIdUseCase.kt
+++ app/src/main/java/com/example/todolistcompose/domain/useCases/todos/GetTodoByIdUseCase.kt
@@ -1,9 +1,6 @@
 package com.example.todolistcompose.domain.useCases.todos
 
-import com.example.todolistcompose.data.todos.TodoModel
-import com.example.todolistcompose.domain.exceptions.TodoNotFoundException
 import com.example.todolistcompose.domain.repos.TodoRepo
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetTodoByIdUseCase @Inject constructor(
